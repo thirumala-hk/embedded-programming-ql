b2951d5b81518ecbcdefbb1e9e1ab43df8e7b6d5
harshith3695 <hthiruma@gmu.edu>
2020-09-09 10:49:16 -0400

    Add chap-01 and index to the repo.

---
+++ chap-01/Debug/subdir_rules.mk
@@ -0,0 +1,11 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: GNU Compiler'
+	"/home/thk3695/Documents/ti/ccs1010/ccs/tools/compiler/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-gcc-7.2.1" -c -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DPART_TM4C123GH6PM -I"/home/thk3695/git-personal/embedded-programming-ql/chap-01" -I"/home/thk3695/Documents/ti/ccs1010/ccs/tools/compiler/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include" -O0 -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -specs="nosys.specs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
---
+++ chap-01/main.c
@@ -0,0 +1,22 @@
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    unsigned int counter = 0;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+
+    return 0;
+}
